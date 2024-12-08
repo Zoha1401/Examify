242c776c51f0cdd711e40dd06d70ba2bc4d30d59
Zoha1401 <zohasurti2004@gmail.com>
2024-12-09 01:58:03 +0530

    Endpoint to delete mcq and programming questions from exam added

--- frontend/src/Components/ExamDetail/Mcq.js
+++ frontend/src/Components/ExamDetail/Mcq.js
@@ -1,17 +1,59 @@
 import React from "react";
+import DeleteIcon from '@mui/icons-material/Delete';
+import ModeEditOutlineIcon from '@mui/icons-material/ModeEditOutline';
+import { useNavigate, useParams } from "react-router-dom";
+import axiosInstance from "../../utils/axiosInstance";
 
-const Mcq = ({ mcq }) => {
+const Mcq = ({ mcq, onDelete }) => {
+  const json = useParams();
+  console.log(json.examId);
+  const examId=json.examId;
+  console.log(examId)
+
+  let navigate=useNavigate();
+  const token = localStorage.getItem("token");
+  console.log(token);
+  if (!token) {
+    alert("You are not authorized please login again");
+    navigate("/examiner-login");
+  }
+
+  
+  const handleDeleteMcq=async()=>{
+     try{
+        const response= await axiosInstance.delete(`/mcqQuestion/deleteMcqQuestion?mcqId=${mcq.mcqId}&examId=${examId}`,
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        )
+
+        if(response.status===200){
+          alert("Mcq is deleted");
+          onDelete(mcq.mcqId)
+        }
+     }
+     catch(error){
+      console.error(
+        "Error deleting:",
+        error.response?.data || error.message
+      );
+      alert("Failed to delete mcq. Please try again.");
+     }
+  }
   return (
     <>
       <div className="flex">
-        <div>{mcq.questionText}</div>
+        <div>{mcq.mcqQuestionText}</div>
+        <div>Correct Answer: {mcq.correctAnswer}</div>
         <div className="flex">
           {mcq.options.map((option, index) => (
             <div key={option.optionId || index}>
               {option.optionText}{" "}
-              {option.isCorrect && <strong>(Correct)</strong>}
+              {option.isCorrect && <strong classname="font-light bg-green-200">(Correct)</strong>}
             </div>
           ))}
+           <ModeEditOutlineIcon/>
+           <DeleteIcon onClick={handleDeleteMcq}/>
         </div>
       </div>
     </>
--- frontend/src/Components/ExamDetail/ProgrammingQuestion.js
+++ frontend/src/Components/ExamDetail/ProgrammingQuestion.js
@@ -1,7 +1,47 @@
 import React from 'react';
 import TestCase from './TestCase';
+import { useNavigate, useParams } from 'react-router-dom';
+import axiosInstance from '../../utils/axiosInstance';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ModeEditOutlineIcon from '@mui/icons-material/ModeEditOutline';
 
-const ProgrammingQuestion = ({pq}) => {
+const ProgrammingQuestion = ({pq, onDelete}) => {
+
+  const json = useParams();
+  console.log(json.examId);
+  const examId=json.examId;
+  console.log(examId)
+
+  let navigate=useNavigate();
+  const token = localStorage.getItem("token");
+  console.log(token);
+  if (!token) {
+    alert("You are not authorized please login again");
+    navigate("/examiner-login");
+  }
+
+  
+  const handleDeleteProgrammingQuestion=async()=>{
+     try{
+        const response= await axiosInstance.delete(`/programmingQuestion/deleteProgrammingQuestion?examId=${examId}&proId=${pq.programmingQuestionId}`,
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        )
+
+        if(response.status===200){
+          alert("Programming Question is deleted");
+          onDelete(pq.programmingQuestionId)
+        }
+     }
+     catch(error){
+      console.error(
+        "Error deleting:",
+        error.response?.data || error.message
+      );
+      alert("Failed to delete question. Please try again.");
+     }
+  }
   return (
     <>
     <div className='flex'>
@@ -12,6 +52,8 @@ const ProgrammingQuestion = ({pq}) => {
            
           ))}
         </div>
+        <ModeEditOutlineIcon/>
+        <DeleteIcon onClick={handleDeleteProgrammingQuestion}/>
     </div>
     </>
   );
